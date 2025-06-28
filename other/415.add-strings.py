ba1be3572eea4add3040f7c4507ca8811ea0b16f
Glen Marco <glen@Glens-MacBook-Pro.local>
2025-06-28 02:09:00 -0400

    added other dir

---
+++ other/415.add-strings.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=415 lang=python3
+#
+# [415] Add Strings
+#
+
+# @lc code=start
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+# @lc code=end
+

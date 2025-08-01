6bb58f9d9b86d66b02948454f98df0b8ccd7e46c
notnine <glenmarcojeremy@gmail.com>
2025-08-01 11:47:43 -0400

    250: not done yet

---
+++ neetcode250/721.accounts-merge.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=721 lang=python3
+#
+# [721] Accounts Merge
+#
+
+# @lc code=start
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        # ToDo
+            
+
+# @lc code=end
+

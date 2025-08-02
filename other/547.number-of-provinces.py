91dc351da962d723cd9d50abef300d29192d0f97
notnine <glenmarcojeremy@gmail.com>
2025-08-02 17:47:47 -0400

    547 updated

--- other/547.number-of-provinces.py
+++ other/547.number-of-provinces.py
@@ -10,12 +10,12 @@ class UnionFind:
         self.parent = [i for i in range(n)]
 
     # return x's root
-    def find(self, x: int) -> int:
+    def find(self, x: int) -> int: # O(log n) time
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
-    def union(self, x: int, y: int) -> None:
+    def union(self, x: int, y: int) -> None: # O(log n) time
         root_x, root_y = self.find(x), self.find(y)
         if root_x != root_y:
             self.parent[root_x] = root_y
@@ -29,18 +29,19 @@ class Solution:
         for i in range(n):
             for j in range(i+1, n):
                 if isConnected[i][j] == 1:
-                    union_find.union(i, j)
+                    union_find.union(i, j) # O(n^2 log n) time
 
         # count distinct number of roots
         num_roots = 0
         visited = set()
         for i in range(n):
-            root = union_find.find(i)
+            root = union_find.find(i) # O(n log n) time
             if root not in visited:
                 num_roots += 1
                 visited.add(root)
         
         return num_roots
 
+        # O(n log n) time, O(n) space
 # @lc code=end
 

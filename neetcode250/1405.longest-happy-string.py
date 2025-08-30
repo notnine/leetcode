4adad351fe9b59886dc19e13ef54368f69bd18f4
notnine <glenmarcojeremy@gmail.com>
2025-08-30 16:22:40 -0400

    1405 template

---
+++ neetcode250/1405.longest-happy-string.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1405 lang=python3
+#
+# [1405] Longest Happy String
+#
+
+# @lc code=start
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        
+# @lc code=end
+

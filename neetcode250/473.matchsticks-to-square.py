6ce1078a967c787d88e9eb80f45b2d891d69dcad
notnine <glenmarcojeremy@gmail.com>
2025-08-31 22:30:16 -0400

    473

---
+++ neetcode250/473.matchsticks-to-square.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=473 lang=python3
+#
+# [473] Matchsticks to Square
+#
+
+# @lc code=start
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        
+# @lc code=end
+

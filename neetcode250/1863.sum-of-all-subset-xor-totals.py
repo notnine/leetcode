0485c921a5eadc021901740d9aba4798639e55ff
notnine <glenmarcojeremy@gmail.com>
2025-08-31 22:29:13 -0400

    1863

--- neetcode250/1863.sum-of-all-subset-xor-totals.py
+++ neetcode250/1863.sum-of-all-subset-xor-totals.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
+        memo = {}
         
+        # return the sum all XOR totals for every subset of nums[i:]
+        def backtrack(i: int, running_xor: int) -> int:
+            if (i, running_xor) in memo:
+                return memo[(i, running_xor)]
+            if i == len(nums):
+                return running_xor
+            
+            take = backtrack(i + 1, running_xor ^ nums[i])
+            skip = backtrack(i + 1, running_xor)
+            memo[(i, running_xor)] = take + skip
+            return take + skip
+
+        return backtrack(0, 0)
 # @lc code=end
 

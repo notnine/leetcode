00ad728b029893c3c73ecb00f975abc83d4b7037
notnine <glenmarcojeremy@gmail.com>
2025-09-02 17:32:57 -0400

    343

--- neetcode250/343.integer-break.py
+++ neetcode250/343.integer-break.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def integerBreak(self, n: int) -> int:
+        dp = [-float('inf')] * (n+1)
+        dp[1] = 1
+
+        # for any number n, ib(n) = max(so_far, i * ib(n-i)) for all i in range(2,n)
+        for i in range(2, n+1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i-j),j * dp[i-j])
         
+        return dp[n]
+
 # @lc code=end
 

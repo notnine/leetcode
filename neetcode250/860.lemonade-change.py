66a25cafef68d6c215e623c67647403b65ae41ab
notnine <glenmarcojeremy@gmail.com>
2025-09-02 17:33:43 -0400

    860

---
+++ neetcode250/860.lemonade-change.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=860 lang=python3
+#
+# [860] Lemonade Change
+#
+
+# @lc code=start
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        
+# @lc code=end
+

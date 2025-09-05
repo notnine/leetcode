ade4c187539b7c10abe3cbea344c0ef213039a1e
notnine <glenmarcojeremy@gmail.com>
2025-09-05 09:01:52 -0400

    560

--- leetcode75/560.subarray-sum-equals-k.py
+++ leetcode75/560.subarray-sum-equals-k.py
@@ -7,25 +7,15 @@
 # @lc code=start
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix_sums = [0] # prefix_sums[i] holds sum(nums[:i])
-        for n in nums:
-            curr = prefix_sums[-1] + n
-            prefix_sums.append(curr)
-
-        # do we need this
-        b_prefix_sums = [0] # b_prefix_sums[i] holds sum(nums[len(nums)-i:])
-        for n in nums[::-1]:
-            curr = prefix_sums[-1] + n
-            prefix_sums.append(curr)
-
-        # check sum of all sub arrays
-        n = len(nums)
+        running_sum = 0 
         res = 0
-        for i in range(n):
-            for j in range(i, n):
-                # get sum(nums[i:j+1])
-                if prefix_sums[j+1] - prefix_sums[i] == k:
-                    res += 1
+        presum_to_count = defaultdict(int)
+        presum_to_count[0] = 1
+        for n in nums:
+            running_sum = running_sum + n
+            if running_sum - k in presum_to_count:
+                res += presum_to_count[running_sum - k]
+            presum_to_count[running_sum] += 1
 
         return res
         

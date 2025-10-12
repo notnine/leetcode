2fe4eab516b96d07355d04859596ad57555db780
notnine <glenmarcojeremy@gmail.com>
2025-10-12 15:16:42 -0400

    1049

--- neetcode250/1049.last-stone-weight-ii.py
+++ neetcode250/1049.last-stone-weight-ii.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
+        # 0/1 knapsack / subset sum problem
+
+        target = sum(stones) // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for w in stones:
+            for s in range(target, w - 1, -1):
+                dp[s] = dp[s] or dp[s - w]
         
+        for s in range(target, -1, -1):
+            if dp[s]:
+                return (sum(stones) - s) - s
 # @lc code=end
 

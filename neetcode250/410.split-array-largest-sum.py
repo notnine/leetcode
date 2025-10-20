ab1212c1b5a40e0fa6ed802e0647bb969ae9a58d
notnine <glenmarcojeremy@gmail.com>
2025-10-20 19:29:49 -0400

    410

---
+++ neetcode250/410.split-array-largest-sum.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=410 lang=python3
+#
+# [410] Split Array Largest Sum
+#
+
+# @lc code=start
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        
+# @lc code=end
+

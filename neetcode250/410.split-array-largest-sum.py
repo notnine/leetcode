2ac3f03a5fff546ede281a00ea7e21d9032e5bbb
notnine <glenmarcojeremy@gmail.com>
2025-10-20 19:53:10 -0400

    410 solution

--- neetcode250/410.split-array-largest-sum.py
+++ neetcode250/410.split-array-largest-sum.py
@@ -8,5 +8,33 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         
+        # return whether we can split nums into k subarrays, where the max([sum(subarr) for each subarr]) <= x
+        def can_split(x: int) -> bool:
+            # greedily maximize curr subarr until its sum is > x, then create a new subarr starting at nums[i]
+            subarrs = 0
+            running_sum = 0
+            for num in nums:
+                if running_sum + num > x:
+                    subarrs += 1
+                    running_sum = num
+                else:
+                    running_sum += num
+            # last subarr is not yet counted in subarrs
+            subarrs += 1
+            return subarrs <= k
+        
+        l, r = max(nums), sum(nums)
+        res = r
+        while l <= r:
+            m = (l + r) // 2
+            if can_split(m):
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return res
+
+
 # @lc code=end
 

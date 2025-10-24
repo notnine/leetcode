a3549dea2286fd85118604edfdd38302b213e1e2
notnine <glenmarcojeremy@gmail.com>
2025-10-24 08:53:45 -0400

    1464 template

---
+++ other/1464.maximum-product-of-two-elements-in-an-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1464 lang=python3
+#
+# [1464] Maximum Product of Two Elements in an Array
+#
+
+# @lc code=start
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+# @lc code=end
+

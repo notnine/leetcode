58b5bf510bdc4c87f2cabaf0f602a03313b29ff2
notnine <glenmarcojeremy@gmail.com>
2025-10-24 08:54:09 -0400

    1464 sol

--- other/1464.maximum-product-of-two-elements-in-an-array.py
+++ other/1464.maximum-product-of-two-elements-in-an-array.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
+        if nums[0] > nums[1]:
+            first_biggest = nums[0]
+            second_biggest = nums[1]
+        else:
+            first_biggest = nums[1]
+            second_biggest = nums[0]
+
+        for num in nums[2:]:
+            if first_biggest <= num:
+                second_biggest = first_biggest
+                first_biggest = num
+
+            elif second_biggest <= num < first_biggest:
+                second_biggest = num
+
+        return (first_biggest - 1) * (second_biggest - 1)
+
 # @lc code=end
 

8151b238003a986766396915fad9fb0e44e9b354
notnine <glenmarcojeremy@gmail.com>
2025-11-10 08:07:53 -0500

    124 sol

--- neetcode250/124.binary-tree-maximum-path-sum.py
+++ neetcode250/124.binary-tree-maximum-path-sum.py
@@ -13,6 +13,23 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = root.val
+
+        # return maxPathSum w/o splitting
+        def dfs(node: Optional[TreeNode]):
+            nonlocal res
+            if not node:
+                return 0
+            
+            left_max = max(dfs(node.left), 0)
+            right_max = max(dfs(node.right), 0)
+
+            # update res if this node w/ splitting is better
+            res = max(res, node.val + left_max + right_max)
+
+            return node.val + max(left_max, right_max)
         
+        dfs(root)
+        return res
 # @lc code=end
 

42d3a78e171e61c14717839d0814c0080d166b2c
notnine <glenmarcojeremy@gmail.com>
2025-11-10 07:53:46 -0500

    124 template

---
+++ neetcode250/124.binary-tree-maximum-path-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=124 lang=python3
+#
+# [124] Binary Tree Maximum Path Sum
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        
+# @lc code=end
+

8df2e2f9d84692a41e60522eb027b9aeeeccabe2
notnine <glenmarcojeremy@gmail.com>
2025-11-12 20:42:28 -0500

    297 sol

--- neetcode250/297.serialize-and-deserialize-binary-tree.py
+++ neetcode250/297.serialize-and-deserialize-binary-tree.py
@@ -12,22 +12,53 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = [] # we use list instead of string because we need seperators b/w values. for e.g what if val is 14
 
-    def serialize(self, root):
-        """Encodes a tree to a single string.
+        # traverse tree pre-order (root, left, right) & put val into res
+        def dfs(node: Optional[TreeNode]) -> None:
+            nonlocal res
+            if not node:
+                res.append('N')
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
         
-        :type root: TreeNode
-        :rtype: str
-        """
-        
+        dfs(root)
+        return ','.join(res)
 
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        res = data.split(',') # get back our res array
+        n = len(res)
+        i = 0 # keeps track of the index our dfs is at
         
-        :type data: str
-        :rtype: TreeNode
-        """
+        # pre-req: i is in bounds
+        # recursively build tree, returning the node at that index
+        def dfs() -> Optional[TreeNode]:
+            nonlocal res, n, i
+            if res[i] == 'N':
+                i += 1
+                return None
+            else:
+                curr = TreeNode(val=int(res[i]))
+                i += 1
+                curr.left = dfs() # attach left subtree
+                curr.right = dfs() # attach right subtree
+                return curr
+
+        return dfs()
         
 
 # Your Codec object will be instantiated and called as such:

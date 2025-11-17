fa9a8a20a1fb908a48d13ec84d492517b6067b3a
notnine <glenmarcojeremy@gmail.com>
2025-11-17 08:20:45 -0500

    ipo template

---
+++ neetcode250/502.ipo.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=502 lang=python3
+#
+# [502] IPO
+#
+
+# @lc code=start
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        
+# @lc code=end
+

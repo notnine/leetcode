855b9c278f945ab45bd16a09454040d649daa4f1
notnine <glenmarcojeremy@gmail.com>
2025-11-18 20:36:47 -0500

    502 ipo sol

--- neetcode250/502.ipo.py
+++ neetcode250/502.ipo.py
@@ -7,6 +7,34 @@
 # @lc code=start
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        # build list of (capital, profit) tuples, sort in ascending order by capital
+
+        capital_profit = []
+        for i in range(len(profits)):
+            capital_profit.append((capital[i], profits[i]))
+        capital_profit.sort()
+        print(capital_profit)
+        i = 0 # points to the first project that is yet to be added into our max heap
+        loop = 0
+        max_heap = []
+
+        # do below k times
+        while loop < k:
+            # maintain max heap that contains projects we can afford (project's capital <= w)
+            while i < len(capital_profit) and capital_profit[i][0] <= w:
+                heapq.heappush(max_heap, -capital_profit[i][1])
+                i += 1
+
+            # greedily pop from max heap, incrementing our w with that project's profit
+            if max_heap:
+                profit = -heapq.heappop(max_heap)
+            else:
+                return w # return if we cannot afford anything
+            w += profit
+
+            loop += 1
         
+        return w
+
 # @lc code=end
 

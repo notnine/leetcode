26383dcb3914372923cd19435649805b0935152c
notnine <glenmarcojeremy@gmail.com>
2025-11-21 10:49:33 -0500

    word break 2 template

---
+++ neetcode250/140.word-break-ii.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=140 lang=python3
+#
+# [140] Word Break II
+#
+
+# @lc code=start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+# @lc code=end
+

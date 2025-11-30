ce16d003bb0e202638faa7cc83b222d7bd3fc9bb
notnine <glenmarcojeremy@gmail.com>
2025-11-29 20:29:54 -0500

    stone game 1 sol 2

--- neetcode250/877.stone-game.py
+++ neetcode250/877.stone-game.py
@@ -7,29 +7,21 @@
 # @lc code=start
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        
-        # given the game state, return whether alice wins. pre-req: l & r are in bounds
-        def dp(alice_points: int, bob_points: int, alice_turn: bool, l: int, r: int) -> bool:
-            if l == r:
-                if alice_turn:
-                    alice_points += piles[l]
-                else:
-                    bob_points += piles[l]
-                return alice_points > bob_points
-            
-            if alice_turn:
-                # alice takes head
-                take_head = dp(alice_points + piles[l], bob_points, not alice_turn, l + 1, r)
-                # alice takes tail
-                take_tail = dp(alice_points + piles[r], bob_points, not alice_turn, l, r - 1)
-                return take_head or take_tail
-            else:
-                # bob takes head
-                take_head = dp(alice_points, bob_points + piles[l], not alice_turn, l + 1, r)
-                # bob takes tail
-                take_tail = dp(alice_points, bob_points + piles[r], not alice_turn, l, r - 1)
-                return take_head or take_tail
+        memo = {}
 
-        return dp(0, 0, True, 0, len(piles) - 1)
+        # return best score difference of current player
+        def dp(l: int, r: int) -> int:
+            if (l, r) in memo:
+                return memo[(l, r)]
+            if l == r:
+                return piles[l]
+            
+            memo[(l, r)] = max(
+                piles[l] - dp(l + 1, r),
+                piles[r] - dp(l, r - 1)
+            )
+            return memo[(l, r)]
+        
+        return True if dp(0, len(piles) - 1) > 0 else False
 # @lc code=end
 

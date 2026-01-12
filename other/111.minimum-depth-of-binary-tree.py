9f9a6f6403b6ade5d32c244fab98b8937b74eb12
notnine <glenmarcojeremy@gmail.com>
2026-01-12 18:54:56 -0500

    111 sol

--- other/111.minimum-depth-of-binary-tree.py
+++ other/111.minimum-depth-of-binary-tree.py
@@ -11,8 +11,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
         
+        curr_lvl = deque([root])
+        depth = 0
+
+        while curr_lvl:
+            depth += 1
+            next_lvl = deque([])
+            for _ in range(len(curr_lvl)):
+                curr_node = curr_lvl.popleft()
+                if not curr_node.left and not curr_node.right:
+                    return depth
+                if curr_node.left:
+                    next_lvl.append(curr_node.left)
+                if curr_node.right:
+                    next_lvl.append(curr_node.right)
+            curr_lvl = next_lvl
+            next_lvl = deque([])
+        
+        return depth
+        
+            
+
+
+
 # @lc code=end
 

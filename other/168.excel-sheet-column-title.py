f053ca90601c2355d5b6579137a81cd26d9f3d0e
notnine <glenmarcojeremy@gmail.com>
2026-01-15 15:31:01 -0500

    168 sol

--- other/168.excel-sheet-column-title.py
+++ other/168.excel-sheet-column-title.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
+        letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        res = ''
+
+        while columnNumber > 0:
+            columnNumber -= 1
+            curr = columnNumber % 26
+            res = letters[curr] + res
+            columnNumber //= 26
         
+        return res
 # @lc code=end
 

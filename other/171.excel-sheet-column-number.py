4cf1b84171b5f944b5720aefcc7cacbf97baed78
notnine <glenmarcojeremy@gmail.com>
2026-01-19 18:45:04 -0500

    171 template

---
+++ other/171.excel-sheet-column-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=171 lang=python3
+#
+# [171] Excel Sheet Column Number
+#
+
+# @lc code=start
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        
+# @lc code=end
+

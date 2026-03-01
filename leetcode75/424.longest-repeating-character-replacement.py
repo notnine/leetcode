b8b301667170b5d2d6f29399fd0c29672aed2e83
notnine <glenmarcojeremy@gmail.com>
2026-03-01 15:41:10 -0500

    424 redid sol

--- leetcode75/424.longest-repeating-character-replacement.py
+++ leetcode75/424.longest-repeating-character-replacement.py
@@ -5,28 +5,35 @@
 #
 
 # @lc code=start
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l, n = 0, len(s)
-        mf = 0
+        freqs = defaultdict(int)
+        mf = 0 # count of most freq. char
+        l, r = 0, 0
         res = 0
-        window = defaultdict(int) # counter for current window
 
-        for r in range(n):
-            # expand window
-            window[s[r]] += 1
-
-            # update most frequent char f
-            mf = max(mf, window[s[r]])
-
-            # while window invalid, shrink til valid
-            while (r - l + 1) - mf > k: # invalid
-                window[s[l]] -= 1
+        # notice we do not have to udpate mf because we only update mf to keep track of res
+        while r < len(s):
+            freqs[s[r]] += 1
+            mf = max(mf, freqs[s[r]])
+            # while invalid, shrink till valid
+            window_size = r - l + 1
+            while window_size - mf > k:
+                freqs[s[l]] -= 1
                 l += 1
+                window_size -= 1
 
-            # update res
-            res = max(res, r - l + 1)
-
+            # at this point we have a valid window
+            res = max(res, window_size)
+            r += 1
+    
         return res
 
 # @lc code=end

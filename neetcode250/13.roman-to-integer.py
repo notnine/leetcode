644c34a6708270086f08fb7de47fe28d1ceb1f66
notnine <glenmarcojeremy@gmail.com>
2026-03-22 17:38:30 -0400

    13 roman to int sol

---
+++ neetcode250/13.roman-to-integer.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        val = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+
+        res = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and val[s[i]] < val[s[i + 1]]:
+                res -= val[s[i]]
+            else:
+                res += val[s[i]]
+
+        return res
+# @lc code=end
+

b65e077d2bff8dc74d5155e82375889a3519ff05
notnine <glenmarcojeremy@gmail.com>
2026-03-29 17:38:19 -0400

    43 sol

--- neetcode250/43.multiply-strings.py
+++ neetcode250/43.multiply-strings.py
@@ -7,24 +7,31 @@
 # @lc code=start
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
         
         if len(num2) > len(num1):
             num1, num2 = num2, num1
         
         list1, list2 = list(num1)[::-1], list(num2)[::-1]
 
-        # todo: we're just continuing to append to the list, we need to actually "add" to the res. use indices, it will help.
-        carry = 0
-        res = []
-        for i in list2:
-            for j in list1:
-                res.append(str((int(i) * int(j) + carry) % 10)) # last bit goes into res
-                carry = (int(i) * int(j) + carry) // 10 # first bit goes into carry
-            #
+        res = [0] * (len(list1)+len(list2))
 
-        res = ''.join(res[::-1])
-        if carry > 0:
-            res = str(carry) + res
+        # list2 is shorter, and is placed at the "bottom" in the mult. board
+        for j in range(len(list2)):
+            for i in range(len(list1)):
+                product = int(list2[j])*int(list1[i]) + res[i+j]
+                res[i+j] = product % 10
+                res[i+j+1] += product // 10
+
+        for i in range(len(res)):
+            res[i] = str(res[i])
+
+        res = res[::-1]
+        i = 0 # first non zero index
+        while res[i] == '0':
+            i += 1
+        res = ''.join(res[i:])
 
         return res
 # @lc code=end

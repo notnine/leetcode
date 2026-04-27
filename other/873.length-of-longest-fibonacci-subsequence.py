0bd1b3cc7246db4bbb2f4b4ccf9b310b4f78c088
notnine <glenmarcojeremy@gmail.com>
2026-04-27 18:23:38 -0400

    top down dp

--- other/873.length-of-longest-fibonacci-subsequence.py
+++ other/873.length-of-longest-fibonacci-subsequence.py
@@ -8,21 +8,29 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
         best = 0
-        arr_set = set(arr)
         n = len(arr)
-
-        for i in range(n-1):
-            for j in range(i+1, n):
-                prev, curr = arr[i], arr[j]
-                nxt = prev + curr
-                l = 2
-
-                while nxt in arr_set:
-                    l += 1
-                    prev, curr = curr, nxt
-                    nxt = prev + curr
-                    best = max(best, l)
+        memo = {}
+        arr_set = set(arr)
+        num_to_i = {num: i for i, num in enumerate(arr)}
         
-        return best
+        # return max number we can add to make fib seubsequence starting at i,j
+        def dfs(i: int, j: int) -> int:
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if arr[i] + arr[j] in num_to_i:
+                new_j = num_to_i[arr[i] + arr[j]]
+                res = 1 + dfs(j, new_j)
+                memo[(i, j)] = res
+                return res
+            
+            return 0
+
+
+        for i in range(n - 1):
+            for j in range(i+1, n):
+                best = max(best, 2 + dfs(i, j))
+        
+        return best if best > 2 else 0
 # @lc code=end
 

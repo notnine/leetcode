ab03e87e8a554ad0bdd0ae18369010c1435da54c
notnine <glenmarcojeremy@gmail.com>
2026-08-20 08:24:14 -0400

    3016

--- other/3016.minimum-number-of-pushes-to-type-word-ii.py
+++ other/3016.minimum-number-of-pushes-to-type-word-ii.py
@@ -1,29 +1,15 @@
-#
-# @lc app=leetcode id=3016 lang=python3
-#
-# [3016] Minimum Number of Pushes to Type Word II
-#
+import math
 
-# @lc code=start
-from collections import Counter
-class Solution:
-    def minimumPushes(self, word: str) -> int:
-        # from the num of unique chars, how many can we fit into 8 buttons
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if val * val == num: # a number is a smart number iff it is a perfect square
+        return True
+    return False
 
-        # get all the unique characters, get all their frequencies
-        # sort frequencies in descending order. We want to map the more frequent chars to less pushes
-        # for each button (freq), take the num of pushes we need and sum it all up
-
-        char_to_freq = Counter(word)
-        freqs = sorted(char_to_freq.values(), reverse=True)
-        res = 0
-
-        for i, f in enumerate(freqs):
-            pushes = ((i // 8) + 1) * f
-            res += pushes
-        
-        return res
-
- 
-# @lc code=end
-
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")

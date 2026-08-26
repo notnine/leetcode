ad593ea6de13a89fd027fac50e9bb12d3e85c5ab
notnine <glenmarcojeremy@gmail.com>
2026-08-26 10:18:24 -0400

    1496

---
+++ other/1496.path-crossing.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=1496 lang=python3
+#
+# [1496] Path Crossing
+#
+
+# @lc code=start
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        visited = set()
+        visited.add((0,0))
+        x, y = 0, 0
+
+        for c in path:
+            if c == 'N':
+                y += 1
+            elif c == 'S':
+                y -= 1
+            elif c == 'E':
+                x += 1
+            else:
+                x -= 1
+
+            if (x, y) in visited:
+                return True
+            visited.add((x, y))
+            
+        print(visited)
+        return False
+# @lc code=end
+

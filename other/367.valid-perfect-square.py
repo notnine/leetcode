51a4855a03951e716f593acd07c7948949294197
notnine <glenmarcojeremy@gmail.com>
2026-09-07 15:35:17 -0400

    367

---
+++ other/367.valid-perfect-square.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=367 lang=python3
+#
+# [367] Valid Perfect Square
+#
+
+# @lc code=start
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        
+        # bin search
+        l, r = 0, (num // 2) + 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if m * m == num:
+                return True
+            elif m * m > num:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return False
+# @lc code=end
+

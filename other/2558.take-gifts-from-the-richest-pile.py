99143838b84088c89915ce84841dbe30da7d2f8f
notnine <glenmarcojeremy@gmail.com>
2026-09-08 17:23:13 -0400

    2558

---
+++ other/2558.take-gifts-from-the-richest-pile.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=2558 lang=python3
+#
+# [2558] Take Gifts From the Richest Pile
+#
+
+# @lc code=start
+import heapq
+import math
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        heap = []
+
+        for gift in gifts:
+            heapq.heappush(heap, -gift)
+        
+        for _ in range(k):
+            largest = -(heapq.heappop(heap))
+            largest = math.floor(math.sqrt(largest))
+            heapq.heappush(heap, -largest)
+        
+        return sum(heap) * -1  
+# @lc code=end
+
